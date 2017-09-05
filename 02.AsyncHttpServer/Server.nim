11bd7229a87e3746158b22b679e0e5f9c8393a80
Benjamin N. Summerton <def.pri.pub@gmail.com>
2017-09-04 23:55:36 -0400

    Almost have it...

--- 02.AsyncHttpServer/Server.nim
+++ 02.AsyncHttpServer/Server.nim
@@ -1,18 +1,24 @@
 # Filename:  Server.nim
 # Author:    Benjamin N. Summerton <define-private-public> @def-pri-pub
 # License:   Unlicense (http://unlicense.org/)
+#
+# TODO mention strfmt in the tutorial page
 
 import asyncDispatch
 import asynchttpserver
 
+
+# Create the server object and state data
 var
   server = newAsyncHttpServer()
   pageViews = 0
   requestCount = 0
+let port = 8000
 
 
-# TODO document
 proc mkPage(viewCount: int; disableSubmit: bool): string=
+  ## Creates a page for us to display.  In goes a view count and a toggle button
+  ## to disable the server, and out comes HTML
   let disableStr = if disableSubmit: "disabled"
                    else:             ""
   return """
@@ -23,17 +29,47 @@ proc mkPage(viewCount: int; disableSubmit: bool): string=
   </head>
   <body>
     <p>Page Views:""" & $viewCount & """</p>
-    <form method="post" action="shutdown">"
+    <form method="post" action="shutdown">
       <input type="submit" value="Shutdown" """ & disableStr & """>
     </form>
   </body>
 </html>
   """
 
-echo mkPage(2, true)
-#
-#proc handleRequest(req: Request) {.async.}=
-#  await req.respond(Http200, "Hello World!")
-#
-#waitFor server.serve(Port(8000), handleRequest)
-#
+
+proc handleRequest(req: Request) {.async.}=
+  ## TODO document
+  var killServer = false
+
+  # Incoming!  print some data
+  requestCount += 1
+  echo("Request #: " & $requestCount)
+  echo(req.url.path)
+  echo(req.reqMethod)
+  echo(req.hostname)
+  echo(req.headers["User-Agent"])
+  echo("")
+
+  # If the `shutdown` url was requested w/ a POST method, then shutdown the server after serving the page
+  if (req.reqMethod == HttpPost) and (req.url.path == "/shutdown"):
+    echo("Shutdown requested")
+    killServer = true
+
+  # Increment the view counter (if a browser doesn't request `favicon.ico`)
+  if req.url.path != "/favicon.ico":
+    pageViews += 1
+
+  # Kill the server?
+  if killServer:
+    server.close()
+
+  # Write a response
+  var headers = newHttpHeaders()
+  headers.add("Content-Type", "text/html")
+  await req.respond(Http200, mkPage(pageViews, killServer), headers)
+
+
+# Handle requests
+echo("Listening for connections on http://localhost:" & $port & "/")
+waitFor server.serve(Port(port), handleRequest)
+
